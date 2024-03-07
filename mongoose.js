2b484fae12f5b134ab5e36166c7efdaa990b0161
Gouravgour123 <gourg1899@gmail.com>
2024-03-07 13:48:09 +0530

    detele

--- mongoose.js
+++
@@ -1,4 +0,0 @@
-const { default: mongoose } = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/CodesForTomorrow")
-.then(()=>console.log("Connected")).catch(()=>console.log("Not Connected"))
